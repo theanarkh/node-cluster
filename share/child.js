a62469dbfb6c8cfa1258ccd7269ede89cf8bb913
theanarkh <2923878201@qq.com>
2020-12-23 00:43:38 +0800

    Update child.js

--- share/child.js
+++ share/child.js
@@ -2,5 +2,5 @@ const net = require('net');
 process.on('message', (message, handle) => {
 	net.createServer(() => {
 		console.log(process.env.index, 'receive connection');
-	}).listen({handle, port: 11111});
+	}).listen({handle});
 });
